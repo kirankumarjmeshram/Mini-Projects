5d4ae54b55ff8b4630bf9448ec5282c38c93f75a
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-10-23 22:55:00 +0530

    made some Improvement

--- import_export(foodApp)/foodApp1/header.js
+++ import_export(foodApp)/foodApp1/header.js
@@ -1,15 +1,15 @@
 function navbar(){
     return `<nav class="nav">
     <div>
-    <h3><a href="/searchReciepe.html">Search Reciepes</a></h3>
+    <h3><a href="/searchRecipe.html">Search Reciepes</a></h3>
     <i class="material-icons">search</i>
     </div>
     <div>
-        <h3><a href="/recOfDay.html">Recipies of the day</a></h3>
+        <h3><a href="/recipeOfTheDay.html">Recipies of the day</a></h3>
         <i class="material-icons">restaurant</i>
     </div>
     <div>
-    <h3><a href="/showLatRec.html">Latest Recipies</a></h3>
+    <h3><a href="/showLatestReceipe.html">Latest Recipies</a></h3>
     <i class="material-icons">lunch_dining</i>
     </div>
     </nav>`;
