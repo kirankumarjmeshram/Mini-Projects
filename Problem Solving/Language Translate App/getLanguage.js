abfa8808078b3b49809311516ca192365fac2ef1
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-10-29 11:25:23 +0530

    options are created succesfully

---
+++ Problem Solving/Language Translate App/getLanguage.js
@@ -0,0 +1,36 @@
+async function getLanguage(){
+    let res = await fetch('https://libretranslate.de/languages');
+    let data = await res.json();
+    appendLanguages(data)
+    appendLanguages1(data)
+    //console.log("data:", data);
+}
+
+function appendLanguages(data){
+    let parent = document.getElementById("languageList");
+
+    data.forEach((el)=>{
+        let options = document.createElement("option");
+
+        options.value = el.code;
+        options.innerText =el.name;
+
+        parent.append(options);
+
+    })
+}
+
+function appendLanguages1(data){
+    let parent= document.getElementById("languageList1");
+
+    data.forEach((el)=>{
+        let options = document.createElement("option");
+
+        options.value=el.code;
+        options.innerText = el.name;
+        parent.append(options);
+    })
+}
+
+getLanguage()
+
